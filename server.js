ccaa4fcf0b5a6bb791e8b072c256d7999a1d29a2
jacobcurry <jacob.a.curry@gmail.com>
2022-12-10 11:20:20 -0500

    changed server.js

--- server.js
+++ server.js
@@ -22,6 +22,20 @@ app.use(methodOverride("_method"));
 //   next();
 // });
 
+app.use((req, res, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT"
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
+
 app.use(
   cors({
     origin: [
