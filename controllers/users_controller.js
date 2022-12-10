b5f198be3a63b782f95858450f9b2e574448f2c4
jacobcurry <jacob.a.curry@gmail.com>
2022-12-10 11:58:11 -0500

    chnage server.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,45 +4,53 @@ const users = require("../models/users");
 const router = express.Router();
 const User = require("../models/users");
 
-router.get("/", (req, res) => {
-  res.send(req.session.currentUser);
+// router.get("/", (req, res) => {
+//   res.send(req.session.currentUser);
+// });
+
+// router.post("/createaccount", (req, res) => {
+//   req.body.password = bcrypt.hashSync(
+//     req.body.password,
+//     bcrypt.genSaltSync(10)
+//   );
+//   User.create(req.body, (err, createdUser) => {
+//     if (err) {
+//       res.json(err.message);
+//     } else {
+//       req.session.currentUser = createdUser;
+
+//       res.json(req.session.currentUser);
+//     }
+//   });
+// });
+
+// router.put("/login", (req, res) => {
+//   User.findOne({ email: req.body.email }, (err, foundUser) => {
+//     if (err) {
+//       res.json(err.message);
+//     } else {
+//       if (!foundUser) {
+//         res.json({
+//           message: "We could not find that email, please try again.",
+//         });
+//       } else if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+//         req.session.currentUser = createdUser;
+//         res.json(req.session.currentUser);
+//       } else {
+//         res.json({
+//           message: "Password was incorrect, please try again.",
+//         });
+//       }
+//     }
+//   });
+// });
+
+router.post("/login", async (req, res) => {
+  res.json({ mssg: "login user" });
 });
 
-router.post("/createaccount", (req, res) => {
-  req.body.password = bcrypt.hashSync(
-    req.body.password,
-    bcrypt.genSaltSync(10)
-  );
-  User.create(req.body, (err, createdUser) => {
-    if (err) {
-      res.json(err.message);
-    } else {
-      req.session.currentUser = createdUser;
-
-      res.json(req.session.currentUser);
-    }
-  });
-});
-
-router.put("/login", (req, res) => {
-  User.findOne({ email: req.body.email }, (err, foundUser) => {
-    if (err) {
-      res.json(err.message);
-    } else {
-      if (!foundUser) {
-        res.json({
-          message: "We could not find that email, please try again.",
-        });
-      } else if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-        req.session.currentUser = createdUser;
-        res.json(req.session.currentUser);
-      } else {
-        res.json({
-          message: "Password was incorrect, please try again.",
-        });
-      }
-    }
-  });
+router.post("/createaccount", async (req, res) => {
+  res.json({ mssg: "signup user" });
 });
 
 module.exports = router;
