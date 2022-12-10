6312c4b3cc3eadfc2eb0ddf50e7f88464751fdad
jacobcurry <jacob.a.curry@gmail.com>
2022-12-10 08:56:26 -0500

    trying to fix cors header issue

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(
   cors({
     origin: [
