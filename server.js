268c80f7f9175c01ce94352032a4f0e8aa964ef0
jacobcurry <jacob.a.curry@gmail.com>
2022-12-10 09:01:53 -0500

    cors fix

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", [
+    "http://localhost:3000",
+    "https://admirable-froyo-7c545e.netlify.app",
+  ]);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
